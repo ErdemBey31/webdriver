88844d45b7b7e86e1e0bfc9a0529e2c47da47d37
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-03-31 09:40:25 +0300

    Add files via upload

---
+++ site.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+op = Options()
+op.add_argument("--no-sandbox")
+op.add_argument("--headless")
+#op.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=op)
+toplam = 0
+
+try:
+  driver.get("https://codesandbox.io/p/devbox/sweet-http-62rlpc")
+  toplam += 1
+  print(f"Toplam: {toplam}")
+except Exception as e:
+  print(e)  
+while True:
+  driver.refresh()
+  time.sleep(15)
+  pass
